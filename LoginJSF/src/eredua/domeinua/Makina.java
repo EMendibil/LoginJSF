2c3b21cb3e05edbe6bdd488218818f83690f1cf7
EMendibil <83004847+EMendibil@users.noreply.github.com>
2021-12-02 12:16:08 +0100

    Lab3 gehio

--- LoginJSF/src/eredua/domeinua/Makina.java
+++ LoginJSF/src/eredua/domeinua/Makina.java
@@ -5,32 +5,33 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 import javax.persistence.Entity;
+
 @Entity
 public class Makina {
-@Id
-private int kodea;
-private String izena;
-@ManyToMany
-private Set<Erabiltzailea> erabiltzaileak;
-public int getKodea() {
-return kodea;
-}
-public void setKodea(int kodea) {
-this.kodea = kodea;
-}
-public String getIzena() {
-return izena;
-}
-public void setIzena(String izena) {
-this.izena = izena;
-}
-public Set<Erabiltzailea> getErabiltzaileak() {
-return erabiltzaileak;
-}
-public void setErabiltzaileak(Set<Erabiltzailea> erabiltzaileak) {
-this.erabiltzaileak = erabiltzaileak;
-}
-public String toString() {
-return kodea + "/" + izena;
-}
+	@Id
+	private int kodea;
+	private String izena;
+	@ManyToMany
+	private Set<Erabiltzailea> erabiltzaileak;
+	public int getKodea() {
+		return kodea;
+	}
+	public void setKodea(int kodea) {
+		this.kodea = kodea;
+	}
+	public String getIzena() {
+		return izena;
+	}
+	public void setIzena(String izena) {
+		this.izena = izena;
+	}
+	public Set<Erabiltzailea> getErabiltzaileak() {
+		return erabiltzaileak;
+	}
+	public void setErabiltzaileak(Set<Erabiltzailea> erabiltzaileak) {
+		this.erabiltzaileak = erabiltzaileak;
+	}
+	public String toString() {
+		return kodea + "/" + izena;
+	}
 }
